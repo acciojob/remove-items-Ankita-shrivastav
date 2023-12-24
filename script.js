7369331ba8759f1b034af6873cf9459454183507
Ankita-shrivastav <93003257+Ankita-shrivastav@users.noreply.github.com>
2023-12-25 00:55:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const colorSelect=document.getElementById('colorSelect');
+const removeButton=document.getElementById('removeButton');
+removeButton.addEventListener('click',function(){
+	const selectedOption=colorSelect.options[colorSelect.selectedIndex];
+	colorSelect.removeChild(selectedOption);
+});
+
+
