0af41390f32818f12ca7bb048e0b0ca87bd46916
Ankita-shrivastav <93003257+Ankita-shrivastav@users.noreply.github.com>
2023-12-25 01:19:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-//your JS code here. If required.
-const colorSelect=document.getElementById('colorSelect');
-const removeButton=document.getElementById('removeButton');
-removeButton.addEventListener('click',function(){
-	const selectedOption=colorSelect.value;
-	const optionToRemove=colorSelect.querySelector('option[value="${selectedOption}"]');
-	if(optionToRemove){
-		colorSelect.removeChild(optionToRemove);
-	}
+const removeButton = document.querySelector('input[type="button"]');
+const colorSelect = document.getElementById('colorSelect');
+
+removeButton.addEventListener('click', function() {
+  const selectedOption = colorSelect.value;
+  const optionToRemove = colorSelect.querySelector(option[value="${selectedOption}"]);
+
+  if (optionToRemove) {
+    colorSelect.removeChild(optionToRemove);
+  }
 });
-
-
